8e482be26edc590c3d5635a4461d78b7646927ac
Muhammed Shaker <muhammedshaker.me@gmail.com>
2025-11-07 22:57:01 -0800

    add mock shots data

---
+++ data/shots.jsx
@@ -0,0 +1,95 @@
+export  const shots = [
+    {
+        "thumbnail": {
+            "src": "https://cdn.dribbble.com/userupload/45607163/file/982fe0973e11dba98ec9e0cbd6b7eb60.png?format=webp&resize=400x300&vertical=center",
+            "alt": "Design Exploration - Editorial Style bold branding card clean design desktop editorial farmer fruits landing page layout magazine marketing page photography typography ui ux vegetables website whitespace"
+        },
+        "link": "/shots/26742119-Design-Exploration-Editorial-Style",
+        "title": "Design Exploration - Editorial Style",
+        "description": "",
+        "author": {
+            "name": "Paperpillar",
+            "avatar": "https://cdn.dribbble.com/users/1047455/avatars/small/316d7dcb011d5b29b8960194c3982a54.png?1721788289"
+        },
+        "likes": 35,
+        "views": 1900
+    },
+    {
+        "thumbnail": {
+            "src": "https://cdn.dribbble.com/userupload/45596433/file/42bfd80e12ea81c6c9f07a9bb1ee097e.png?format=webp&resize=400x300&vertical=center",
+            "alt": "HR Management App — Smart Workforce & Device Management app dashboard ui data visualization design system device management employee management enterprise app fintech ui forkforce management hr app hr platform ios map ui minimal ui mobile app mobile ui product design ui ux design"
+        },
+        "link": "/shots/26738754-HR-Management-App-Smart-Workforce-Device-Management",
+        "title": "HR Management App — Smart Workforce & Device Management",
+        "description": "",
+        "author": {
+            "name": "Nixtio",
+            "avatar": "https://cdn.dribbble.com/users/1057940/avatars/small/f068710cc8e5fcebcbcd8f1f75d1b4ef.gif?1732126488"
+        },
+        "likes": 236,
+        "views": 17100
+    },
+    {
+        "thumbnail": {
+            "src": "https://cdn.dribbble.com/userupload/45602156/file/2a8fd23efe63222187e4f58382b8aeae.png?format=webp&resize=400x300&vertical=center",
+            "alt": "FrameLabs Logo Design bold brand design brand identity brand system branding business card confident icon logo logo design logotype minimal modern stationery symbol tech technology techware visual identity wordmark"
+        },
+        "link": "/shots/26740629-FrameLabs-Logo-Design",
+        "title": "FrameLabs Logo Design",
+        "description": "",
+        "author": {
+            "name": "Mark Smith, Smi.",
+            "avatar": "https://cdn.dribbble.com/users/238836/avatars/small/70c01476ac24c54e954bb5b773fdf132.png?1644591628"
+        },
+        "likes": 30,
+        "views": 4500
+    },
+    {
+        "thumbnail": {
+            "src": "https://cdn.dribbble.com/userupload/45606810/file/c432b5522be2d332bfe52485019534f7.jpg?format=webp&resize=400x300&vertical=center",
+            "alt": "Mountain Plus adventure logo modern mountain nature simple"
+        },
+        "link": "/shots/26742006-Mountain-Plus",
+            "title": "Mountain Plus",
+        "description": "",
+        "author": {
+            "name": "Catur Argi",
+            "avatar": "https://cdn.dribbble.com/users/1144167/avatars/small/c262d2a5e9dd1b2fca7a5fc4e3ddf88a.png?1702264899"
+        },
+        "likes": 11,
+        "views": 732
+    },
+
+    {
+        "thumbnail": {
+            "src": "https://cdn.dribbble.com/userupload/45605747/file/9c2e2ae787dcd38f42def44e584b02fd.jpg?format=webp&resize=400x300&vertical=center",
+            "alt": "Passenger Clothing 2025 Collection: Blanket design adventure apparel blanket branding camp design forest graphic design illustration landscape mountains nature outdoor outdoors river shirt t shirt"
+        },
+        "link": "/shots/26741691-Passenger-Clothing-2025-Collection-Blanket-design",
+        "title": "Passenger Clothing 2025 Collection: Blanket design",
+        "description": "",
+        "author": {
+            "name": "Lisa McCormick / Made by Lisa Marie",
+            "avatar": "https://cdn.dribbble.com/users/512167/avatars/small/914a70cc40ffafd5f89342fa21596c6d.png?1718396342",
+            "badge": "Pro"
+        },
+        "likes": 13,
+        "views": 2000
+    },
+    {
+        "thumbnail": {
+            "src": "https://cdn.dribbble.com/userupload/45599834/file/92cabe0397a5beda81e7a0224b82e09a.png?crop=186x139-1414x1061&format=webp&resize=400x300&vertical=center",
+            "alt": "Raptor logo brand brand designer branddesigner branding branding designer design dino graphic design letter r logo logo design logo designer logodesign logodesigner logotype r logo raptor raptor logo"
+        },
+        "link": "/shots/26739942-Raptor-logo",
+        "title": "Raptor logo",
+        "description": "",
+        "author": {
+            "name": "Andrii Kovalchuk 🇺🇦 Brand designer",
+            "avatar": "https://cdn.dribbble.com/users/1700393/avatars/small/4321e40fb501ef264d3c9d9d996ad652.png?1698337873",
+            "badge": "Pro"
+        },
+        "likes": 53,
+        "views": 5300
+    }
+]
